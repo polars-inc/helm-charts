72424c1032161d9ad6964022c15efd54fd37a680
Jop Zitman <jop@polars.tech>
2025-12-18 10:55:50 +0100

    reduce helm-docs doc amount and add ci

--- generate-schema.sh
+++ generate-schema.sh
@@ -12,10 +12,14 @@ fi
 
 ln -sf _definitions-v${DEFINITIONS_VERSION}.json _definitions.json
 
-helm schema --chart-search-root=charts
+helm schema --chart-search-root=charts --helm-docs-compatibility-mode --log-level=debug --skip-auto-generation required
 
 for CHART in charts/*; do
   echo "Updating $CHART"
+
   jq '. + input' _definitions.json $CHART/values.schema.json | sponge $CHART/values.schema.json
+
+  sed -i '' "s|\"../../_definitions.json#|\"|g" $CHART/values.schema.json
+
 done
 
